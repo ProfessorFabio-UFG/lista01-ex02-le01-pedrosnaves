9e3f708f2b442ac60f22c33960cbca7c4577f2e5
pedrosnaves <pedrosnaves@gmail.com>
2025-04-04 21:47:39 -0300

    Update Cadastro2.java

--- ex02/Cadastro2.java
+++ ex02/Cadastro2.java
@@ -4,54 +4,16 @@ public class Cadastro2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Moradores[] moradores = new Moradores[100];
-        int total = 0;
+        Moradores[] moradores = new Moradores[5];
+        moradores[0] = new Moradores ("Claudio", "70864285744", "62995874412", "12/10/1956", "masculino", "A", 102, 1565);
+        moradores[1] = new Moradores ("Ramon", "40122587466", "62980521466", "14/03/1945", "masculino", "B", 903, 2548);
+        moradores[2] = new Moradores ("Jorge", "00125863633", "62980566624", "10/11/1999", "masculino", "B", 801, 3305);
+        moradores[3] = new Moradores ("Abadia", "85966410002", "62999568874", "20/12/1975", "feminino", "C", 505, 4023);
+        moradores[4] = new Moradores ("Hilda", "11235684715", "62988451965", "19/09/1988", "feminino", "A", 302, 5900);
 
-        String resposta;
-        do {
-            System.out.println("\nCadastro do morador" + (total + 1));
-
-            System.out.print("Nome do morador: ");
-            String nome = sc.nextLine();
-
-            System.out.print("CPF do morador: ");
-            String cpf = sc.nextLine();
-
-
-            System.out.print("Telefone para contato do morador: ");
-            String contato = sc.nextLine();
-
-
-            System.out.print("Data de nascimento do morador (DD/MM/AAAA): ");
-            String data_nasc = sc.nextLine();
-
-            System.out.print("Sexo do morador (M ou F): ");
-            String sexo = sc.nextLine();
-
-            System.out.print("Bloco do morador: ");
-            String bloco = sc.nextLine();
-
-            System.out.print("Apartamento do morador: ");
-            int apartamento = sc.nextInt();
-
-            System.out.print("Código de acesso do morador: ");
-            int cod_acesso = sc.nextInt();
-            sc.nextLine();
-
-            moradores[total] = new Moradores(nome, cpf, contato, data_nasc, sexo, bloco, apartamento, cod_acesso);
-            total++;
-
-            System.out.println("Morador cadastrado com sucesso!\n");
-
-            System.out.print("Deseja cadastrar outro morador? (S/N): ");
-            resposta = sc.nextLine();
-        } while (resposta.equalsIgnoreCase("S"));
-
-        System.out.println("\n===== Lista de moradores cadastrados =====");
-        for (int i = 0; i < total; i++) {
+        for (int i = 0; i < 5; i++){
             System.out.println(moradores[i]);
         }
-
-
     }
+
 }
